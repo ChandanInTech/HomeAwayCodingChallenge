6227e3f0c78b76d6961bddea4d5ea1482cc4083f
Chandan Kurella <kurella.chandu4@gmail.com>
2018-04-15 15:31:47 -0400

    clean up

--- app/src/main/java/com/example/kurella/homeawaycodingchallenge/model/Events.java
+++ app/src/main/java/com/example/kurella/homeawaycodingchallenge/model/Events.java
@@ -2,232 +2,40 @@ package com.example.kurella.homeawaycodingchallenge.model;
 
 public class Events {
 
-    private String date_tbd;
-
-    private String time_tbd;
-
     private String datetime_utc;
 
-    //private Stats stats;
-
-    private String score;
-
-    private String datetime_tbd;
-
-    private String announce_date;
-
-    private String[] links;
-
     private Venue venue;
-
-    private String type;
-
-    private String url;
-
     private String id;
 
     private String short_title;
 
-    private String visible_until_utc;
-
     private Performers[] performers;
 
     private String title;
-
-    private String datetime_local;
-
-    private String created_at;
-
-    //private Taxonomies[] taxonomies;
-
-    public String getDate_tbd ()
-    {
-        return date_tbd;
-    }
-
-    public void setDate_tbd (String date_tbd)
-    {
-        this.date_tbd = date_tbd;
-    }
-
-    public String getTime_tbd ()
-    {
-        return time_tbd;
-    }
-
-    public void setTime_tbd (String time_tbd)
-    {
-        this.time_tbd = time_tbd;
-    }
-
     public String getDatetime_utc ()
     {
         return datetime_utc;
     }
-
-    public void setDatetime_utc (String datetime_utc)
-    {
-        this.datetime_utc = datetime_utc;
-    }
-
-//    public Stats getStats ()
-//    {
-//        return stats;
-//    }
-//
-//    public void setStats (Stats stats)
-//    {
-//        this.stats = stats;
-//    }
-
-    public String getScore ()
-    {
-        return score;
-    }
-
-    public void setScore (String score)
-    {
-        this.score = score;
-    }
-
-    public String getDatetime_tbd ()
-    {
-        return datetime_tbd;
-    }
-
-    public void setDatetime_tbd (String datetime_tbd)
-    {
-        this.datetime_tbd = datetime_tbd;
-    }
-
-    public String getAnnounce_date ()
-    {
-        return announce_date;
-    }
-
-    public void setAnnounce_date (String announce_date)
-    {
-        this.announce_date = announce_date;
-    }
-
-    public String[] getLinks ()
-    {
-        return links;
-    }
-
-    public void setLinks (String[] links)
-    {
-        this.links = links;
-    }
-
     public Venue getVenue ()
     {
         return venue;
     }
-
-    public void setVenue (Venue venue)
-    {
-        this.venue = venue;
-    }
-
-    public String getType ()
-    {
-        return type;
-    }
-
-    public void setType (String type)
-    {
-        this.type = type;
-    }
-
-    public String getUrl ()
-    {
-        return url;
-    }
-
-    public void setUrl (String url)
-    {
-        this.url = url;
-    }
-
     public String getId ()
     {
         return id;
     }
-
-    public void setId (String id)
-    {
-        this.id = id;
-    }
-
     public String getShort_title ()
     {
         return short_title;
     }
-
-    public void setShort_title (String short_title)
-    {
-        this.short_title = short_title;
-    }
-
-    public String getVisible_until_utc ()
-    {
-        return visible_until_utc;
-    }
-
-    public void setVisible_until_utc (String visible_until_utc)
-    {
-        this.visible_until_utc = visible_until_utc;
-    }
-
     public Performers[] getPerformers ()
     {
         return performers;
-    }
-
-    public void setPerformers (Performers[] performers)
-    {
-        this.performers = performers;
     }
 
     public String getTitle ()
     {
         return title;
     }
-
-    public void setTitle (String title)
-    {
-        this.title = title;
-    }
-
-    public String getDatetime_local ()
-    {
-        return datetime_local;
-    }
-
-    public void setDatetime_local (String datetime_local)
-    {
-        this.datetime_local = datetime_local;
-    }
-
-    public String getCreated_at ()
-    {
-        return created_at;
-    }
-
-    public void setCreated_at (String created_at)
-    {
-        this.created_at = created_at;
-    }
-
-//    public Taxonomies[] getTaxonomies ()
-//    {
-//        return taxonomies;
-//    }
-//
-//    public void setTaxonomies (Taxonomies[] taxonomies)
-//    {
-//        this.taxonomies = taxonomies;
-//    }
 
 }
